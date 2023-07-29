77939f3eb3e294ba7676b35a9b614a80bb3ad87c
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-07-29 19:50:57 -0300

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+echo
+echo -e "\e[31m------------------------- | Github: https://github.com/suchsoak | -------------------------\e[0m"
+echo
+
+sleep 5 > /dev/null
+
+if gem install colorize os time; then
+        echo "Colorize, os and time"
+    else 
+        echo "error!"
+
+fi
